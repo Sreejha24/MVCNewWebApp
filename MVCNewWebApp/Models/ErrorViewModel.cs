eaa387733f9079e804a5c3e6744af2c77dab35c6
Sreejha24 <saisreejha04@gmail.com>
2020-12-29 03:41:27 -0800

    MVCProjects

---
+++ MVCNewWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCNewWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
